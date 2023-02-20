39e94925d074b7a5d6de515312d3f605441855b8
JLev189 <jacklevins@hotmail.com>
2023-02-20 19:34:26 +0000

    initial commit lab2

---
+++ Lab2/app.js
@@ -0,0 +1,9 @@
+const http = require("http");
+const portNumber = 8081;
+
+http.createServer(function (request, response) {
+response.writeHead(200, {'Content-Type': 'text/plain'});
+response.end('Hello World\n’);
+}).listen(portNumber);
+
+console.log(`Server running at http://127.0.0.1:${portNumber}/`);
