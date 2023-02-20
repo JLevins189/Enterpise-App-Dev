153dc5e84f55d59dfa2f60297ff0aabef3697e92
JLev189 <jacklevins@hotmail.com>
2023-02-20 19:36:43 +0000

    quick fix

--- Lab2/app.js
+++ Lab2/app.js
@@ -1,9 +1,11 @@
 const http = require("http");
 const portNumber = 8081;
 
-http.createServer(function (request, response) {
-response.writeHead(200, {'Content-Type': 'text/plain'});
-response.end('Hello World\n’);
-}).listen(portNumber);
+http
+  .createServer(function (request, response) {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end("Hello World\n");
+  })
+  .listen(portNumber);
 
 console.log(`Server running at http://127.0.0.1:${portNumber}/`);
