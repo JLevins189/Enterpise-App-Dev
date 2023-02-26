f5f329a90895124adf4ffeb1b68ccf5d42a7abd0
JLev189 <jacklevins@hotmail.com>
2023-02-26 18:47:25 +0000

    allow cors + port = 8080

--- Lab2/backend/app.js
+++ Lab2/backend/app.js
@@ -1,15 +1,17 @@
-const serverPort = 3000;
+const serverPort = 8080;
 const express = require("express");
 const routes = require("./routes");
 const bodyParser = require("body-parser");
-const { request } = require("express");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
+const allowedOrigins = ["http://localhost:3000"];
+
+app.use(cors({ origin: allowedOrigins }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
-
 app.use(routes);
 
 app.listen(serverPort, () => {
