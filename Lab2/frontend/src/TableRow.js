692c312d0dbe603caaba1fc1e4cf5ab676fe454b
JLev189 <jacklevins@hotmail.com>
2023-03-08 14:13:14 +0000

    add capital

--- Lab2/frontend/src/TableRow.js
+++ Lab2/frontend/src/TableRow.js
@@ -1,13 +1,23 @@
 function CountryTableRow(props) {
-  const { country, continent, coastline, currency, domain, flag, row } = props;
+  const {
+    country,
+    capital,
+    continent,
+    coastline,
+    currency,
+    domain,
+    flag,
+    row,
+  } = props;
   return (
     <tr>
       <td id={`${row}-1`}>{country}</td>
-      <td id={`${row}-2`}>{continent}</td>
-      <td id={`${row}-3`}>{coastline}</td>
-      <td id={`${row}-4`}>{currency}</td>
-      <td id={`${row}-5`}>{domain}</td>
-      <td id={`${row}-6`}>
+      <td id={`${row}-2`}>{capital}</td>
+      <td id={`${row}-3`}>{continent}</td>
+      <td id={`${row}-4`}>{coastline}</td>
+      <td id={`${row}-5`}>{currency}</td>
+      <td id={`${row}-6`}>{domain}</td>
+      <td id={`${row}-7`}>
         <img src={flag} width={100}></img>
       </td>
     </tr>
