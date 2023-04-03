cabefe07852694e9a2f23a610158c5b80d143ba3
JLev189 <jacklevins@hotmail.com>
2023-04-03 13:26:45 +0100

    template endpoints

--- Lab3/backend/controllers/colourController.js
+++ Lab3/backend/controllers/colourController.js
@@ -14,12 +14,42 @@ router.get("/", (req, res) => {
   res.setHeader("Expires", new Date(Date.now() + 3600 * 1000).toUTCString());
   res.sendFile(path.join(__dirname, "..", "public", "data.json"));
 });
+
 router.get("/:id", (req, res) => {
   const id = req.params.id;
-  console.log(`Request Received for colour with id ${id}`);
+  console.log(`GET request received for colour with id ${id}`);
   res.setHeader("Cache-Control", "public, max-age=3600");
   res.setHeader("Expires", new Date(Date.now() + 3600 * 1000).toUTCString());
   res.send(req.params.id); //TODO update to object
+});
+
+router.post("/", (req, res) => {
+  console.log("Request Received for new colour");
+  res.send("1"); //TODO update to object
+});
+
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(`PUT request received for colour with id ${id}`);
+  res.send("1"); //TODO update to object
+});
+
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(`DELETE request received for colour with id ${id}`);
+  res.send("1"); //TODO update to object
+});
+
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(`DELETE request received for colour with id ${id}`);
+  res.send("1"); //TODO update to object
+});
+
+router.delete("/", (req, res) => {
+  const id = req.params.id;
+  console.log(`DELETE request received for colour with id ${id}`);
+  res.send("1"); //TODO update to object
 });
 
 module.exports = router;
