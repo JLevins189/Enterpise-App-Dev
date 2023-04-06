e67a24d19699609febc855b6a3a9e0c422e9028a
JLev189 <jacklevins@hotmail.com>
2023-04-06 17:03:35 +0100

    increase size

--- Lab3/frontend/src/util/LoadingSpinner.js
+++ Lab3/frontend/src/util/LoadingSpinner.js
@@ -3,8 +3,8 @@ import Spinner from "react-bootstrap/Spinner";
 
 function LoadingSpinner() {
   return (
-    <Container className="d-flex justify-content-center my-5">
-      <Spinner animation="grow" variant="primary" />
+    <Container className="d-flex justify-content-center my-5 py-5">
+      <Spinner animation="grow" variant="primary" className="table-cell" />
     </Container>
   );
 }
