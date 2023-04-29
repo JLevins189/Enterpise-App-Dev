52ceb03d27d0580d7d47eb4d846a625a15e921b6
JLev189 <jacklevins@hotmail.com>
2023-04-29 20:15:52 +0100

    add key

--- Lab4/lab4-frontend/src/listComponents/ProductDataList.jsx
+++ Lab4/lab4-frontend/src/listComponents/ProductDataList.jsx
@@ -5,7 +5,10 @@ function ProductDataList({ productData }) {
   return (
     <Row className="mx-1 mx-lg-5">
       {productData.map((productElement) => (
-        <ProductListElement productElement={productElement} />
+        <ProductListElement
+          productElement={productElement}
+          key={productElement?._id}
+        />
       ))}
     </Row>
   );
